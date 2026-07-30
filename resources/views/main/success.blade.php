b80b80b99702993e219e483031488a8e06aacdff
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-07-30 23:04:27 +0700

    feat(success view) : adding countdown for button back

--- resources/views/main/success.blade.php
+++ resources/views/main/success.blade.php
@@ -55,7 +55,14 @@
 
             <hr class="my-6 border-gray-100">
 
-            <a href="{{ route('main.index') }}"
+            <div id="timer-info" class="text-xs font-semibold text-violet-600 bg-violet-50 p-3 rounded-xl mb-2">
+                Countdown button back <span id="countdown-text">10</span> detik
+            </div>
+
+            <div id="btn-back" class="hidden">
+
+                <a href="{{ route('main.index') }}"
+                id="btn-back"
                 class="inline-flex items-center justify-center gap-2 w-full bg-violet-700 hover:bg-violet-800 text-white font-bold text-sm py-3 px-4 rounded-xl shadow-md shadow-violet-700/10 transition-all duration-200 active:scale-[0.98]">
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0 l7-7m-7 7h18">
@@ -63,7 +70,34 @@
                 </svg>
                 Kembali ke Halaman Utama
             </a>
-
+            
+            </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            let time = 20
+            const textCountdown = document.getElementById('countdown-text')
+            const timerinfo = document.getElementById('timer-info')
+            const button = document.getElementById('btn-back')
+
+            const intervaltimer = setInterval(() => {
+                time --
+                if (textCountdown) {
+                    textCountdown.textContent = time
+                }
+
+                if (time <= 0) {
+                    clearInterval(intervaltimer)
+                }
+            }, 1000);
+
+            setTimeout(() => {
+                timerinfo.classList.add('hidden')
+
+                button.classList.remove('hidden')
+            }, 20000);
+        })
+    </script>
 @endsection
